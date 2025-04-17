49297ed61ed9e9c52e072d829c72b1a971b8ac56
Paras Mittal <parasmittal05>
2025-04-17 21:35:41 +0530

    Your commit message here

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,35 +1,30 @@
 import React, { useEffect, useState } from "react";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import axios from "axios";
-import Cards from "./Cards";
 
+import axios from "axios";
+
+import Cards from "./Cards";
 function Freebook() {
   const [book, setBook] = useState([]);
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const getBook = async () => {
       try {
-<<<<<<< HEAD
         const res = await axios.get("http://localhost:4001/book");
-=======
-        const res = await axios.get("mongodb://localhost:27017/bookStore/book");
 
->>>>>>> 551c4a0ec72b1b88c65c66625aa9f6e55e663c48
         const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
         setBook(data);
       } catch (error) {
         console.log(error);
-      } finally {
-        setLoading(false);
       }
     };
     getBook();
   }, []);
 
-  const settings = {
+  var settings = {
     dots: true,
     infinite: false,
     speed: 500,
@@ -51,6 +46,7 @@ function Freebook() {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
+          initialSlide: 2,
         },
       },
       {
@@ -62,31 +58,27 @@ function Freebook() {
       },
     ],
   };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-      <div>
-        <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
-          corporis nulla non suscipit, iure neque earum?
-        </p>
-      </div>
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
+            corporis nulla non suscipit, iure neque earum?
+          </p>
+        </div>
 
-      <div>
-        <Slider {...settings}>
-          {book.map((item) => (
-            <Cards item={item} key={item.id} />
-          ))}
-        </Slider>
+        <div>
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
 export default Freebook;
