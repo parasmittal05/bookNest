4170c7cb1cc9021870c6a951c17ce38c9a285af5
parasmittal05 <97421711+parasmittal05@users.noreply.github.com>
2025-04-18 13:11:49 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("https://booknest-backend-xbka.onrender.com/user/signup", userInfo)
+      .post("https://booknest-eiuc.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
