c3813a16cf8d567e5e24e90672e0fd77404c8374
Paras Mittal <parasmittal05>
2025-04-18 12:15:06 +0530

    backend live link

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -4,12 +4,10 @@ import Login from "./Login";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import toast from "react-hot-toast";
-
 function Signup() {
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from?.pathname || "/";
-
   const {
     register,
     handleSubmit,
@@ -22,8 +20,6 @@ function Signup() {
       email: data.email,
       password: data.password,
     };
-<<<<<<< HEAD
-=======
     await axios
       .post("https://booknest-bac.onrender.com/user/signup", userInfo)
       .then((res) => {
@@ -54,110 +50,81 @@ function Signup() {
               >
                 ✕
               </Link>
->>>>>>> 3ca5a3f518893a2a66db6a3b43a9f1abbb43bf55
 
-    try {
-      const res = await axios.post(
-        "https://booknest-bac.onrender.com/user/signup",
-        userInfo
-      );
-
-      if (res.data) {
-        toast.success("Signup Successfully");
-        localStorage.setItem("Users", JSON.stringify(res.data.user));
-        
-        // Adding a small delay (optional)
-        setTimeout(() => {
-          navigate(from, { replace: true });
-        }, 10); // 100ms delay before redirecting
-      }
-    } catch (err) {
-      if (err.response) {
-        console.error(err);
-        toast.error("Error: " + err.response.data.message);
-      }
-    }
-  };
-
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="w-[600px]">
-        <div className="modal-box">
-          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
-            <Link
-              to="/"
-              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-            >
-              ✕
-            </Link>
-
-            <h3 className="font-bold text-lg">Signup</h3>
-
-            {/* Full Name */}
-            <div className="mt-4 space-y-2">
-              <span>Name</span>
-              <input
-                type="text"
-                placeholder="Enter your fullname"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
-                {...register("fullname", { required: true })}
-              />
-              {errors.fullname && (
-                <span className="text-sm text-red-500">This field is required</span>
-              )}
-            </div>
-
-            {/* Email */}
-            <div className="mt-4 space-y-2">
-              <span>Email</span>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
-                {...register("email", { required: true })}
-              />
-              {errors.email && (
-                <span className="text-sm text-red-500">This field is required</span>
-              )}
-            </div>
-
-            {/* Password */}
-            <div className="mt-4 space-y-2">
-              <span>Password</span>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
-                {...register("password", { required: true })}
-              />
-              {errors.password && (
-                <span className="text-sm text-red-500">This field is required</span>
-              )}
-            </div>
-
-            {/* Signup Button */}
-            <div className="flex justify-around mt-4">
-              <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-                Signup
-              </button>
-              <p className="text-xl">
-                Have account?{" "}
-                <button
-                  type="button"
-                  className="underline text-blue-500 cursor-pointer"
-                  onClick={() =>
-                    document.getElementById("my_modal_3").showModal()
-                  }
-                >
-                  Login
+              <h3 className="font-bold text-lg">Signup</h3>
+              <div className="mt-4 space-y-2">
+                <span>Name</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Enter your fullname"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("fullname", { required: true })}
+                />
+                <br />
+                {errors.fullname && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              {/* Email */}
+              <div className="mt-4 space-y-2">
+                <span>Email</span>
+                <br />
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("email", { required: true })}
+                />
+                <br />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              {/* Password */}
+              <div className="mt-4 space-y-2">
+                <span>Password</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Enter your password"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("password", { required: true })}
+                />
+                <br />
+                {errors.password && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              {/* Button */}
+              <div className="flex justify-around mt-4">
+                <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                  Signup
                 </button>
-                <Login />
-              </p>
-            </div>
-          </form>
+                <p className="text-xl">
+                  Have account?{" "}
+                  <button
+                    className="underline text-blue-500 cursor-pointer"
+                    onClick={() =>
+                      document.getElementById("my_modal_3").showModal()
+                    }
+                  >
+                    Login
+                  </button>{" "}
+                  <Login />
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
